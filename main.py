840cebc0b216d744e75561fbe2f5e41b49173e4c
Michael Reid <cybercritter@comcast.net>
2024-03-23 08:36:37 -0600

    fix libpcap interface

--- main.py
+++ main.py
@@ -1,41 +1,3 @@
-<<<<<<< Updated upstream
-import ctypes, sys
-from ctypes.util import find_library
-
-if sys.platform == "darwin":
-    _pcap = ctypes.cdll.LoadLibrary(find_library("libpcap"))
-elif sys.platform == "linux2":
-    _pcap = ctypes.cdll.LoadLibrary("libpcap.so")
-
-errbuf = ctypes.create_string_buffer(256)
-pcap_close = _pcap.pcap_close
-
-pcap_lookupdev = _pcap.pcap_lookupdev
-pcap_lookupdev.restype = ctypes.c_char_p
-dev = pcap_lookupdev(errbuf)
-print(f"Attching to {dev}")
-
-pcap_create = _pcap.pcap_create
-pcap_create.restype = ctypes.c_void_p
-handle = pcap_create(dev, errbuf)
-
-if not handle:
-    print("failed creating handler:",errbuf)
-    exit()
-
-pcap_activate = _pcap.pcap_activate
-pcap_activate.argtype = [ctypes.c_void_p]
-
-errbuf = pcap_activate(handle)
-print(errbuf)
-
-# monitor mode
-pcap_can_set_rfmon = _pcap.pcap_can_set_rfmon
-pcap_can_set_rfmon.argtypes = [ctypes.c_void_p]
-print("can rfmon:",pcap_can_set_rfmon(handle))
-
-||||||| Stash base
-=======
 """
 Packet capture utility
 """
@@ -104,4 +66,3 @@ elif errono == 1:
 
 if __name__ == "__main__":
     main()
->>>>>>> Stashed changes
