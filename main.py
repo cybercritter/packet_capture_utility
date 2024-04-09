75596a121a470dc6ba1135d66f7d2cc47a3dfc70
Michael Reid <cybercritter@comcast.net>
2024-04-09 15:59:16 -0600

    updates to use pyshark

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-"""
-This application captures IP packets from the os ip stack
+import pyshark
+import time
+import json
 
-"""
+TIMEOUT = 15
+start = time.time()
+# Capture packets on the 'en0' interface
+capture = pyshark.LiveCapture(interface='en0')
 
-if __name__ == '__name__':
-    print(f'Starting packet capturing - {__name__}')
+# Loop to continuously capture and process packets
+for packet in capture.sniff_continuously():
+    print(f'Packet length: {len(packet)}')
+    print(f"Protocol: {packet.transport_layer}")
+    print(f"Ethernet Header: {packet['eth'].src}")
+    json.dump(packet, open("my.json", "w"))
+
+    if time.time() - start > TIMEOUT:
+        break
+
+capture.clear()
+capture.close()
