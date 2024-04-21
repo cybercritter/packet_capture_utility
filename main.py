671c731dd28685571a83fa2e3c1f8f0d27219271
Michael Reid <cybercritter@comcast.net>
2024-04-20 19:34:38 -0600

    update

--- main.py
+++ main.py
@@ -9,19 +9,27 @@ Example:
     $ python capture_script.py
 """
 
-from packtlib.packt_capture import CustomPcap
+from packtlib.packt_capture import Capture
 
 if __name__ == '__main__':
     # Initialize CustomPcap object for capturing packets from interface
     # 'en0' with a timeout of 30 seconds
-    pcap_test = CustomPcap('eno0', timeout=30, packet_count=1000)
+    pcap_test = Capture('en0', timeout=10, packet_count=1000)
     print(f'Starting Capture on interface: {pcap_test.iface}')
-    buffer = pcap_test.start_capture()
+    pcap_test.start_capture()
+
     print('End Capture')
 
     # Print the number of packets captured
-    print(f'Number of packets captured: {len(buffer)}')
+    buffers = pcap_test.get_packet()
 
+    try:
+        print(f'Number of packets captured: {len(buffers)}')
+    except Exception as e:
+        print(f'Error: {e}')
+
+    # packet = pcap_test.get_packet()
+    # print(packet)
     # Uncomment the following block to print details of each captured packet
     # for packet in pcap_test.buffer:
     #     try:
--- packtlib/packt_capture.py
+++ packtlib/packt_capture.py
@@ -4,18 +4,16 @@ Packets captured will be only IP packets
 """
 import pyshark
 import time
-from utilities.customstack import CustomStack
 
 
-class Capture(CustomStack):
+class Capture:
     """
     A class for custom packet capture and analysis.
 
     Attributes:
         iface (str): The interface to capture packets from.
         timeout (int): The timeout value for packet capture (default is 10 seconds).
-        stack (list): A list to store captured packets.
-        stack (CustomStack): An instance of CustomStack for managing the buffer.
+        buffer (list): A list to store captured packets.
         start_time (float): The start time of the capture process.
 
     Methods:
@@ -26,7 +24,7 @@ class Capture(CustomStack):
         start_capture(): Start capturing packets and adding them to the buffer.
     """
 
-    def __init__(self, iface, packet_count, timeout=10, stack=None):
+    def __init__(self, iface, packet_count, timeout=10):
         """
         Initialize a CustomPcap object.
 
@@ -34,33 +32,52 @@ class Capture(CustomStack):
             iface (str): The interface to capture packets from.
             timeout (int, optional): The timeout value for packet capture (default is 10 seconds).
         """
-        self.stack = stack
+        self.packet_summary = {}
         self.iface = iface
-        self.timeout = timeout
+        self.timeout = 30
         self.packet_count = packet_count
         self.start_time = time.time()
+
+    def add_packet_to_buffer(self, packet):
+        """
+        Get details of a packet, including protocol type,
+        source and destination addresses,
+        source and destination ports, and packet timestamp.
+
+        Args:
+            packet: The packet for which to retrieve details.
+
+        Returns:
+            str: A formatted string containing the packet details.
+        """
+
+        protocol = packet.transport_layer
+
+        self.packet_summary.update({'packet_time': packet.sniff_time})
+        self.packet_summary.update({'protocol': protocol})
+        #
+        if hasattr(packet, "ip"):
+            self.packet_summary.update({'source_address': packet.ip.src})
+            self.packet_summary.update({'source_port': packet[packet.transport_layer].srcport})
+            self.packet_summary.update({'destination_address': packet.ip.dst})
+            self.packet_summary.update({'destination_port': packet[packet.transport_layer].dstport})
+            self.packet_summary.update({'packet_time': packet.sniff_time})
 
     @property
     def empty(self):
         """
-        Check if the buffer stack is empty.
+        Check if the buffer is empty.
 
         Returns:
             bool: True if the buffer stack is empty, False otherwise.
         """
-        return self.stack.isempty()
+        if len(self.packet_summary) == 0:
+            return True
+        else:
+            return False
 
-    def _add_packet_to_buffer(self, packet) -> None:
-        """
-        Adds a packet to the buffer stack.
-
-        Args:
-            packet: The packet to add to the buffer stack.
-
-        Returns:
-            None
-        """
-        self.stack.push(item=packet)
+    def get_packets(self):
+            print(self.packet_summary)
 
     def get_packet(self):
         """
@@ -70,8 +87,8 @@ class Capture(CustomStack):
             tuple: A tuple containing the packet and port number.
         """
         try:
-            if not self.stack.isempty():
-                return self.stack.pop()
+            if not self.empty:
+                print(self.packet_summary.pop('packet_time'), self.packet_summary.pop('protocol'))
         except TypeError as e:
             print(e)
 
@@ -79,23 +96,26 @@ class Capture(CustomStack):
         """
         Start capturing packets and adding them to the buffer.
         """
-        count = 0
-        capture = pyshark.LiveCapture(interface=self.iface)
-        try:
-            print(f"Capturing packets for {self.timeout} seconds")
-            capture.sniff(packet_count=self.packet_count)
-        except TypeError as e:
-            print(e)
+        # while True:
+        #     if self.start_time + self.timeout < time.time():
+        #         break
+        #
+        #     # Create a LiveCapture object, using the default interface
+        #     capture = pyshark.LiveCapture(interface="en0")
+        #
+        #     # Start capturing packets
+        #     # capture.sniff(packet_count=100)  # Capture packets for 10 seconds, for example
+        #
+        #     # Start capturing packets and apply the callback function to each packet
+        #     capture.apply_on_packets(get_packet_details)
 
-            capture.close()
+        #     # Create a LiveCapture object, using the default interface
+        capture = pyshark.LiveCapture(interface="en0")
 
-            for packet in capture._packets:
-                self._add_packet_to_buffer(packet)
-                count += 1
-                print(f"{count} packets captured", end="\r")
+        for packet in capture.sniff_continuously(packet_count=10):
+            # Print information about each captured packet
+            self.add_packet_to_buffer(packet)
 
-            if len(capture._packets) <= 0:
-                raise
-            return self.stack
-
-
+            # Check if the timeout has been reached
+            if time.time() - self.start_time >= self.timeout:
+                break
