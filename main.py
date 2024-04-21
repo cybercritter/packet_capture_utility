0eb5a219cbe4dd0dacefd565465f9563f481e01f
Michael Reid <cybercritter@comcast.net>
2024-04-21 10:38:40 -0600

    parsing packets correctlu now added write to json file

--- main.py
+++ main.py
@@ -10,32 +10,31 @@ Example:
 """
 
 from packtlib.packt_capture import Capture
+import json
 
 if __name__ == '__main__':
     # Initialize CustomPcap object for capturing packets from interface
     # 'en0' with a timeout of 30 seconds
-    pcap_test = Capture('en0', timeout=10, packet_count=1000)
+    pcap_test = Capture('en0', timeout=10)
     print(f'Starting Capture on interface: {pcap_test.iface}')
     pcap_test.start_capture()
 
     print('End Capture')
 
-    # Print the number of packets captured
-    buffers = pcap_test.get_packet()
-
     try:
-        print(f'Number of packets captured: {len(buffers)}')
+        print(f'Number of packets captured: {pcap_test.packet_num_packet_captured}')
     except Exception as e:
         print(f'Error: {e}')
 
-    # packet = pcap_test.get_packet()
-    # print(packet)
-    # Uncomment the following block to print details of each captured packet
-    # for packet in pcap_test.buffer:
+    # for packet in pcap_test.packet_summary:
     #     try:
-    #         print(pcap_test.get_packet_details(packet))
+    #         if packet is not None:
+    #             print(packet)
     #     except AttributeError:
     #         '''
     #         if Attribute type is not in the attribute list, just ignore
     #         the packet
     #         '''
+    with open('pcap_files/packets.json', 'w+') as outfile:
+        json.dump(pcap_test.packet_summary, outfile, indent=2)
+
--- packtlib/packt_capture.py
+++ packtlib/packt_capture.py
@@ -4,118 +4,58 @@ Packets captured will be only IP packets
 """
 import pyshark
 import time
+from datetime import datetime
 
 
 class Capture:
-    """
-    A class for custom packet capture and analysis.
-
-    Attributes:
-        iface (str): The interface to capture packets from.
-        timeout (int): The timeout value for packet capture (default is 10 seconds).
-        buffer (list): A list to store captured packets.
-        start_time (float): The start time of the capture process.
-
-    Methods:
-        empty (property): Check if the buffer stack is empty.
-        get_packet_details(packet): Get details of a packet.
-        _add_packet_to_buffer(packet): Add a packet to the buffer stack.
-        get_packet(): Remove and return the first packet from the buffer.
-        start_capture(): Start capturing packets and adding them to the buffer.
-    """
-
-    def __init__(self, iface, packet_count, timeout=10):
-        """
-        Initialize a CustomPcap object.
-
-        Args:
-            iface (str): The interface to capture packets from.
-            timeout (int, optional): The timeout value for packet capture (default is 10 seconds).
-        """
-        self.packet_summary = {}
+    def __init__(self, iface, timeout=10):
+        self.packet_summary = []
         self.iface = iface
-        self.timeout = 30
-        self.packet_count = packet_count
+        self.timeout = timeout
+        self.capture = None
         self.start_time = time.time()
 
-    def add_packet_to_buffer(self, packet):
-        """
-        Get details of a packet, including protocol type,
-        source and destination addresses,
-        source and destination ports, and packet timestamp.
+    @staticmethod
+    def add_packet_to_buffer(packet):
 
-        Args:
-            packet: The packet for which to retrieve details.
-
-        Returns:
-            str: A formatted string containing the packet details.
-        """
-
-        protocol = packet.transport_layer
-
-        self.packet_summary.update({'packet_time': packet.sniff_time})
-        self.packet_summary.update({'protocol': protocol})
-        #
-        if hasattr(packet, "ip"):
-            self.packet_summary.update({'source_address': packet.ip.src})
-            self.packet_summary.update({'source_port': packet[packet.transport_layer].srcport})
-            self.packet_summary.update({'destination_address': packet.ip.dst})
-            self.packet_summary.update({'destination_port': packet[packet.transport_layer].dstport})
-            self.packet_summary.update({'packet_time': packet.sniff_time})
+        try:
+            if hasattr(packet, "ip"):
+                return {
+                    "timestamp": packet.sniff_time.timestamp(),
+                    "protocol": packet.highest_layer,
+                    "source": packet.ip.src,
+                    "destination": packet.ip.dst,
+                    "length": packet.length
+                }
+        except TypeError:
+            return None
 
     @property
-    def empty(self):
-        """
-        Check if the buffer is empty.
-
-        Returns:
-            bool: True if the buffer stack is empty, False otherwise.
-        """
+    def is_empty(self):
         if len(self.packet_summary) == 0:
             return True
         else:
             return False
 
-    def get_packets(self):
-            print(self.packet_summary)
-
-    def get_packet(self):
-        """
-        Remove and return the first packet from the buffer.
-
-        Returns:
-            tuple: A tuple containing the packet and port number.
-        """
-        try:
-            if not self.empty:
-                print(self.packet_summary.pop('packet_time'), self.packet_summary.pop('protocol'))
-        except TypeError as e:
-            print(e)
+    @property
+    def packet_num_packet_captured(self):
+        return len(self.packet_summary)
 
     def start_capture(self):
-        """
-        Start capturing packets and adding them to the buffer.
-        """
-        # while True:
-        #     if self.start_time + self.timeout < time.time():
-        #         break
-        #
-        #     # Create a LiveCapture object, using the default interface
-        #     capture = pyshark.LiveCapture(interface="en0")
-        #
-        #     # Start capturing packets
-        #     # capture.sniff(packet_count=100)  # Capture packets for 10 seconds, for example
-        #
-        #     # Start capturing packets and apply the callback function to each packet
-        #     capture.apply_on_packets(get_packet_details)
+        time2 = time.localtime().tm_sec
+        self.capture = pyshark.LiveCapture(interface="en0")
 
-        #     # Create a LiveCapture object, using the default interface
-        capture = pyshark.LiveCapture(interface="en0")
+        self.capture = pyshark.LiveCapture(interface=self.iface)
 
-        for packet in capture.sniff_continuously(packet_count=10):
-            # Print information about each captured packet
-            self.add_packet_to_buffer(packet)
-
+        for packet in self.capture.sniff_continuously():
+            # when a packet is captured
+            self.packet_summary.append(self.add_packet_to_buffer(packet))
+            time1 = int(time.localtime().tm_sec)
+            time_elapsed = time1 - time2
+            if time_elapsed == 1:
+                print(f"#", end='')
             # Check if the timeout has been reached
             if time.time() - self.start_time >= self.timeout:
+                print()
                 break
+            time2 = int(time.localtime().tm_sec)
