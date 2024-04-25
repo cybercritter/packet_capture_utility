83ca342597638e8cb74e9a22afa2240449933162
Michael Reid <cybercritter@comcast.net>
2024-04-24 18:23:39 -0600

    pre final update

--- README.md
+++ README.md
@@ -1,18 +1,55 @@
 ![Pylint](https://github.com/cybercritter/packet_capture_utility/actions/workflows/pylint.yml/badge.svg)
 
+# Author and class information
+Author: Michael Reid
+Final Project: CS 1400 (Spring 2024)
+Instructor: John Adamic
+
 # Packet Capture utility
 Capture packets from the IP stack
 ---
-This app will capture packets from the IP/TCP/UDP stack and log them.<br>
+Capture packets from a specified network interface and write packet summary for ip layer packets to a JSON file.<br>
 
-The packet header information and data will be processed and presented in user readable format.
+The packet header information  will be used to store summary in user readable format.
+****
 
-![IP Header](images/IPv4-header_banner.webp)
+## Python dependencies
+All module dependencies are in the requirements.txt file.<br>
+Use the Python command pip to install the dependencies. <i>pip install -r requirements.txt</><br>
 
-If the protocol is well known the IP/TCP/UDP information and data will be processed and presented in user readable format.
+# Other dependencies
+This program needs the tshark library. The easiest way to acquire it is to install Wireshark<br>
+which can be downloaded from the Wireshark website https://www.wireshark.org/download.html</br>
 
-![TCP/UDP Headers](images/92926-tcp_udp_headers.jpg)
+This app uses the 4.2.4 version. However, it is expected to support later versions.
+***
+## OS's Tested
+This program has been tested on Windows 10 and MacOS Sonoma.
 
-If application type is in the supported list, the app will decode the data for store.
+Linux should be supported but is untested
+***
 
-[ ] Define the supported application types
+## Help and examples of use
+PS C:\Users\cyber\Documents\projects\packet_capture_utility> python .\capture_packets.py -h
+usage: capture_packets.py [-h] -i INTERFACE [-t TIMEOUT] [-o OUTPUT]
+
+Capture packets from a specified network interface and write packet summary for ip layer packets to a JSON file.
+
+options:
+  -h, --help            show this help message and exit
+  -i INTERFACE, --interface INTERFACE
+                        Network interface name (e.g., eth0)
+  -t TIMEOUT, --timeout TIMEOUT
+                        Timeout duration in seconds
+  -o OUTPUT, --output OUTPUT
+                        Output JSON file path
+
+Examples:
+Mac/Linux
+$ python capture_packets.py -i en0 -t 10 -o captured_packets.json
+
+Windows
+$ python capture_packets.py -i Ethernet -t 10 -o captured_packets.json
+PS C:\Users\cyber\Documents\projects\packet_capture_utility>
+
+
---
+++ capture_packets.py
@@ -0,0 +1,63 @@
+#!/bin/python3
+"""
+Capture packets from a specified network interface and write packet summary to a JSON file.
+
+This script initializes a Capture object from the packtlib module to capture packets
+from the specified network interface with a timeout. It then prints the number of
+captured packets and writes the packet summary to a JSON file.
+
+Requirements:
+- packtlib module with Capture class
+
+Usage:
+1. Ensure the required modules are installed. The required modules are in a requirements.txt file
+2. Run the script.
+
+Example:
+    $ python capture_packets.py -i eth0 -t 10 -o captured_packets.json
+"""
+
+import argparse
+import json
+
+# disabled this error as the program runs.
+# pylint: disable=import-error
+from packtlib.capture import Capture
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Capture packets from a specified network '
+                                                 'interface and write packet summary for ip'
+                                                 ' layer packets to a JSON file.',
+                                     epilog='Examples:\n'
+                                            'Mac/Linux\n'
+                                            '$ python capture_packets.py -i en0 -t 10 '
+                                            '-o captured_packets.json\n\n'
+                                            'Windows\n'
+                                            '$ python capture_packets.py -i Ethernet '
+                                            '-t 10 -o captured_packets.json\n',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('-i', '--interface', type=str,
+                        help='Network interface name (e.g., eth0)', required=True)
+    parser.add_argument('-t', '--timeout', type=int,
+                        help='Timeout duration in seconds', default=10)
+    parser.add_argument('-o', '--output', type=str,
+                        help='Output JSON file path', default='pcap_files/packets.json')
+
+    args = parser.parse_args()
+
+    # Initialize Capture object for capturing packets
+    pcap_test = Capture(args.interface, timeout=args.timeout)
+
+    # Start capturing packets
+    print(f'Starting Capture on interface: {pcap_test.iface}')
+    pcap_test.start_capture()
+    print('End Capture')
+
+    # Print the number of packets captured
+    print(f'Number of packets captured: {pcap_test.packet_num_packet_captured}')
+
+    # Write packet summary to a JSON file
+    with open(args.output, 'w+', encoding='utf-8') as outfile:
+        json.dump(pcap_test.packet_summary, outfile, indent=2)
